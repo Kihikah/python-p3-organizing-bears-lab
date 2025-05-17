e89bf32e100fdaa6e5a031490068a519fb7fb591
Kariuki Kihikah <kariukikihikah@gmail.com>
2025-05-17 23:19:40 +0300

    Sql lab bears

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+  ('Mr. Chocolate', 10, 'M', 'Brown', 'Calm', 1),
+  ('Rowdy', 8, 'M', 'Black', 'Aggressive', 1),
+  ('Tabitha', 5, 'F', 'White', 'Calm', 1),
+  ('Sergeant Brown', 12, 'M', 'Dark Brown', 'Serious', 1),
+  ('Melissa', 7, 'F', 'Honey', 'Playful', 1),
+  ('Grinch', 11, 'M', 'Gray', 'Grumpy', 0),
+  ('Wendy', 6, 'F', 'Golden', 'Happy', 1),
+  (NULL, 4, 'M', 'Spotted', 'Quiet', 1);
